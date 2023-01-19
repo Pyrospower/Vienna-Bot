869987e85e0b4112ba0529023c227235749f7a38
Pyrospower <thepassione7@gmail.com>
2023-01-19 23:59:25 +0100

    Commande remindme

---
+++ src/commands/remindme.ts
@@ -0,0 +1,99 @@
+import { SlashCommandBuilder, userMention } from "discord.js";
+import { SlashCommand } from "../types";
+import { createEmbed } from "../utils/embed";
+import { timeToMilliseconds, timeToString } from "../utils/time";
+
+const command: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName("remindme")
+    .setDescription("Envoie un message dans un channel après un certain temps")
+    .addStringOption((option) =>
+      option
+        .setName("reminder")
+        .setDescription("Le rappel à envoyer")
+        .setRequired(true)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("secondes")
+        .setDescription(
+          "Le temps après lequel le message sera envoyé en secondes"
+        )
+        .setRequired(true)
+        .setMinValue(0)
+        .setMaxValue(59)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("minutes")
+        .setDescription(
+          "Le temps après lequel le message sera envoyé en minutes"
+        )
+        .setRequired(false)
+        .setMinValue(0)
+        .setMaxValue(59)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("heures")
+        .setDescription(
+          "Le temps après lequel le message sera envoyé en heures"
+        )
+        .setRequired(false)
+        .setMinValue(0)
+        .setMaxValue(59)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("jours")
+        .setDescription(
+          "Le nombre de jours après lequel le message sera envoyé"
+        )
+        .setRequired(false)
+        .setMinValue(0)
+    ),
+  async execute(interaction) {
+    if (!interaction.channel) return;
+    // Récupère les arguments de la commande
+    const time = {
+      seconds: interaction.options.getNumber("secondes") ?? 0,
+      minutes: interaction.options.getNumber("minutes") ?? 0,
+      hours: interaction.options.getNumber("heures") ?? 0,
+      days: interaction.options.getNumber("jours") ?? 0,
+    };
+    const reminder = interaction.options.getString("reminder");
+
+    // Si le message est trop long, on envoie un message d'erreur
+    if (reminder && reminder.length > 1000) {
+      await interaction.reply({
+        content: "Erreur : le message est trop long.",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    // On envoie un message de confirmation
+    const remindmeTime = timeToString(time);
+
+    const embed = createEmbed(interaction.user);
+    embed.setDescription(`✅ Vous aurez un rappel dans ${remindmeTime}.`);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+
+    // On envoie le message après le temps spécifié
+    const { createdTimestamp } = interaction;
+
+    const reminderEmbed = createEmbed(interaction.user)
+      .setTitle("⏰ Rappel")
+      .setDescription(reminder)
+      .setTimestamp(createdTimestamp);
+
+    setTimeout(async () => {
+      await interaction.user
+        .send({ embeds: [reminderEmbed] })
+        .catch((e) => console.error(e));
+    }, timeToMilliseconds(time));
+  },
+};
+
+export default command;
---
+++ src/utils/time.ts
@@ -0,0 +1,35 @@
+interface Time {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+// Convertit le temps en millisecondes
+export function timeToMilliseconds(time: Time) {
+  const { days = 0, hours = 0, minutes = 0, seconds } = time;
+
+  const timeInMilliseconds = {
+    daysInMilliseconds: days * 24 * 60 * 60 * 1000,
+    hoursInMilliseconds: hours * 60 * 60 * 1000,
+    minutesInMilliseconds: minutes * 60 * 1000,
+    secondsInMilliseconds: seconds * 1000,
+  };
+
+  return Object.values(timeInMilliseconds).reduce((a, b) => a + b);
+}
+
+// Crée une phrase avec le temps
+export function timeToString(time: Time) {
+  const { days = 0, hours = 0, minutes = 0, seconds } = time;
+
+  const daysTxt = days > 0 ? `${days} jour${days > 1 ? "s" : ""} ` : "";
+  const hoursTxt = hours > 0 ? `${hours} heure${hours > 1 ? "s" : ""} ` : "";
+  // prettier-ignore
+  const minutesTxt = minutes > 0 ? `${minutes} minute${minutes > 1 ? "s" : ""} ` : "";
+  const secondsTxt = seconds > 1 ? `${seconds} secondes` : `${seconds} seconde`;
+
+  const sentence = `${daysTxt}${hoursTxt}${minutesTxt}${secondsTxt}`;
+
+  return sentence;
+}
